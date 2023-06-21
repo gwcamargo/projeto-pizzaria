c1cb1c6694bef202ea2bf4ea569f82c337a1d76c
Guilherme Camargo <gwcamargo20@gmail.com>
2023-06-21 11:36:13 -0300

    projeto finalizado

--- README.md
+++
@@ -1,2 +0,0 @@
-# projeto-pizzaria
- Pizzaria online
--- index.js
+++
@@ -1,9 +0,0 @@
-pizzaJson.map((item, index) => {
-    console.log(item)
-
-    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
-
-    document.querySelector('.pizza-area').append(pizzaItem)
-
-    pizzaItem.querySelector('.pizza-item--img img').src = item.img
-})
