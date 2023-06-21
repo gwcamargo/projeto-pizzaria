5d7322b018cdcee146db9f05f3923e24c332e142
Guilherme Camargo <gwcamargo20@gmail.com>
2023-06-21 17:03:12 -0300

    mais algumas alterações do projeto

--- pizzaria-da-nonna02/js/script.js
+++ pizzaria-da-nonna02/js/script.js
@@ -1,6 +1,35 @@
+// funções auxiliares ou úteis
+const seleciona = (elemento) => document.querySelector(elemento)
+const seleciona_todos = (elemento) => document.querySelectorAll(elemento)
+
+const abrir_modal = () => {
+    seleciona('.pizzaWindowArea').style.opacity = 0
+    seleciona('.pizzaWindoArea').style.display = 'flex'
+    setTimeout(() => {
+        seleciona('.pizzaWindowArea').style.opacity = 1
+    }, 150)
+}
+
+const fechar_modal = () => {
+    seleciona('.pizzaWindowArea').style.opacity = 0
+    setTimeout(() => {
+        seleciona('.pizzaWindowArea').style.display = 'none'
+    }, 500)
+}
+
+const botoes_fechar = () => {
+    seleciona_todos('.pizzaInfo--cancelButton, pizzaInfo--cancelMobileButton').forEach((item) => {
+        item.addEventListener('click', fechar_modal)
+    })
+}
+
+// Mapear pizzaJson para gerar listas de pizzas
 pizzaJson.map((item, index ) => {
     //console.log(item)
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
+
+    seleciona('.pizza-area').append(pizzaItem)
+    
     //console.log(pizzaItem)
     document.querySelector('.pizza-area').append(pizzaItem)
 
